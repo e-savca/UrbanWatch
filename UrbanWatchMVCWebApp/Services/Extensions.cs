86b9745bd521628a83e6dfa43fbb8c22f4d8107a
Eugeniu Savca <eusavca@gmail.com>
2023-06-29 22:34:37 +0300

    add presentation

--- UrbanWatchMVCWebApp/Services/Extensions.cs
+++ UrbanWatchMVCWebApp/Services/Extensions.cs
@@ -6,33 +6,33 @@ namespace UrbanWatchMVCWebApp.Services
     public static class Extensions
     {
         public static bool UseDatabase { get; set; }
-        //public static async Task<List<T>> ToListAsync<T>(this IQueryable<T> source)
-        //{
+        public static async Task<List<T>> ToListAsync<T>(this IQueryable<T> source)
+        {
 
-        //    if (UseDatabase)
-        //    {
-        //        return await source.ToListAsync();
-        //    }
-        //    else
-        //    {
-        //        return source.ToList();
-        //    }
-        //}
+            if (UseDatabase)
+            {
+                return await source.ToListAsync();
+            }
+            else
+            {
+                return source.ToList();
+            }
+        }
 
-        //public static async Task<TSource?> FirstOrDefaultAsync<TSource>(
-        //    this IQueryable<TSource> source,
-        //    Expression<Func<TSource, bool>> predicate)
-        //{
-        //    if (UseDatabase)
-        //    {
-        //        return await source.FirstOrDefaultAsync(predicate);
-        //    }
-        //    else
-        //    {
-        //        // in memory
-        //        return source.ToList().FirstOrDefault(predicate.Compile());
+        public static async Task<TSource?> FirstOrDefaultAsync<TSource>(
+            this IQueryable<TSource> source,
+            Expression<Func<TSource, bool>> predicate)
+        {
+            if (UseDatabase)
+            {
+                return await source.FirstOrDefaultAsync(predicate);
+            }
+            else
+            {
+                // in memory
+                return source.ToList().FirstOrDefault(predicate.Compile());
 
-        //    }
-        //}
+            }
+        }
     }
 }
