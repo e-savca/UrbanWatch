4ff9ff741990d5c4596f114c6a89c389d146aa0e
Eugeniu Savca <6307017+e-savca@users.noreply.github.com>
2025-01-13 00:06:43 +0200

    cleand up the main branch

--- UrbanWatchMVCWebApp/Models/ApiModels/TranzyV1Models/StopTimes.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace UrbanWatchMVCWebApp.Models.ApiModels.TranzyV1Models;
-public class StopTimes
-{
-    [JsonProperty("trip_id")] public string? TripId { get; set; }
-    [JsonProperty("stop_id")] public string? StopId { get; set; }
-    [JsonProperty("stop_sequence")] public string? StopSequence { get; set; }
-}
--- UrbanWatchMVCWebApp/Models/DataModels/StopTimes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UrbanWatchMVCWebApp.Models.DataModels;
-public class StopTimes
-{
-    public int Id { get; set; }
-    public string? TripId { get; set; }
-    public string? StopId { get; set; }
-    public string? StopSequence { get; set; }
-}
