ce2ea9affa2398e0b84b1c6ac39007c81eaed15a
Eugeniu Savca <6307017+e-savca@users.noreply.github.com>
2025-01-18 12:38:30 +0200

    -- minor changes

--- react-src/urban-watch/src/App.jsx
+++ react-src/urban-watch/src/App.jsx
@@ -1,20 +1,20 @@
 import Footer from './components/Footer'
-import {BrowserRouter, Route, Routes} from "react-router";
-import Home from "./pages/Home.jsx";
-import NavBar from "./components/NavBar.jsx";
-import RoutesPage from "./pages/RoutesPage.jsx";
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './pages/Home.jsx'
+import RoutesPage from './pages/RoutesPage.jsx'
+import Header from './components/Header.jsx'
 
 function App() {
   return (
-      <BrowserRouter>
-          <NavBar />
-          <Routes>
-              <Route index element={<Home />} />
-              <Route path="/" element={<Home />} />
-              <Route path="/map" element={<RoutesPage />} />
-          </Routes>
-          <Footer />
-      </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<RoutesPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
---
+++ react-src/urban-watch/src/components/Header.jsx
@@ -0,0 +1,183 @@
+import { useState } from 'react'
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import {
+  MapPinIcon,
+  ArrowsRightLeftIcon,
+  MapIcon,
+} from '@heroicons/react/24/outline'
+
+const products = [
+  {
+    name: 'Bus Stop Explorer',
+    description:
+      'Easily locate a bus stop and discover all the live routes and buses passing through it in real-time.',
+    href: '#',
+    icon: MapPinIcon,
+  },
+  {
+    name: 'Route Tracker',
+    description:
+      'Select a specific route to see detailed information about all stops and track buses live along the way.',
+    href: '#',
+    icon: MapIcon,
+  },
+  {
+    name: 'Route Navigator',
+    description:
+      'Plan your trip by choosing starting and ending points. Discover intersecting routes and optimize your journey with ease.',
+    href: '#',
+    icon: ArrowsRightLeftIcon,
+  },
+]
+
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="bg-white">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <a href="/" className="-m-1.5 p-1.5">
+            <h1 className="text-xl font-bold text-black">
+              Urban<span className="text-green-500">Watch</span>
+            </h1>
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+          <Popover className="relative">
+            <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
+              Map
+              <ChevronDownIcon
+                aria-hidden="true"
+                className="size-5 flex-none text-gray-400"
+              />
+            </PopoverButton>
+
+            <PopoverPanel
+              transition
+              className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+            >
+              <div className="p-4">
+                {products.map((item) => (
+                  <div
+                    key={item.name}
+                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50"
+                  >
+                    <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                      <item.icon
+                        aria-hidden="true"
+                        className="size-6 text-gray-600 group-hover:text-indigo-600"
+                      />
+                    </div>
+                    <div className="flex-auto">
+                      <a
+                        href={item.href}
+                        className="block font-semibold text-gray-900"
+                      >
+                        {item.name}
+                        <span className="absolute inset-0" />
+                      </a>
+                      <p className="mt-1 text-gray-600">{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </PopoverPanel>
+          </Popover>
+
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Features
+          </a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Marketplace
+          </a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Company
+          </a>
+        </PopoverGroup>
+      </nav>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="/" className="-m-1.5 p-1.5">
+              <h1 className="text-xl font-bold text-black">
+                Urban<span className="text-green-500">Watch</span>
+              </h1>
+            </a>
+
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                    Product
+                    <ChevronDownIcon
+                      aria-hidden="true"
+                      className="size-5 flex-none group-data-[open]:rotate-180"
+                    />
+                  </DisclosureButton>
+                </Disclosure>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Features
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Marketplace
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Company
+                </a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  )
+}
