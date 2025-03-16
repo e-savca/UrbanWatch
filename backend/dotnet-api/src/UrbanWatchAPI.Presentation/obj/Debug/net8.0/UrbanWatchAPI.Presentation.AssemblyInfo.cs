debf757bb3ed75596865fc0a7a7a3348f545e58e
Eugeniu Savca <6307017+e-savca@users.noreply.github.com>
2025-03-16 20:30:25 +0200

    added gitignore

--- backend/dotnet-api/src/UrbanWatchAPI.Presentation/obj/Debug/net8.0/UrbanWatchAPI.Presentation.AssemblyInfo.cs
+++ backend/dotnet-api/src/UrbanWatchAPI.Presentation/obj/Debug/net8.0/UrbanWatchAPI.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrbanWatchAPI.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b538674b84e18048b784953dd1c5802e79c028c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7febc7558a5e8f4507784398259dc21bb9ae2a11")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrbanWatchAPI.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrbanWatchAPI.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
