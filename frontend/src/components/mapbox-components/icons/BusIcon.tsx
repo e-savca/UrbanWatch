4dc5e600e408eed2a76a4f6fe5337e7e2e7e9499
Eugeniu Savca <6307017+e-savca@users.noreply.github.com>
2025-04-06 15:04:25 +0300

    Moved frontend app to a new repository

--- frontend/src/components/mapbox-components/icons/BusIcon.tsx
+++
@@ -1,9 +0,0 @@
-function BusIcon() {
-  return (
-    <div className="flex items-center justify-center w-12 h-12 bg-blue-600 text-white rounded-full shadow-lg border-2 border-white">
-      <span className="text-2xl">🚌</span>
-    </div>
-  );
-}
-
-export default BusIcon;
